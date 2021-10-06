e062335a659bf53601d6ca417553b35c7d9209fd
Aleena Shibu <54793714+AleenaShibu@users.noreply.github.com>
2021-10-06 12:41:55 +0530

    Create Compute.java

---
+++ Compute.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Scanner;
+class Compute
+int n;
+char ch;
+int s;
+if(ch=='s')
+System.out.println("Square of integer");
+System.out.println("n*n");
+else
+System.out.println("Cube of the integer");
+System.out.println("n*n*n");
+}
+public static void Calculate(int m,int n,char ch)
+{
+if(ch=='p')
+System.out.println("Product of the integer");
+System.out.println("m*n");
+else
+System.Out.println("Add the integer");
+System.out.println("m+n");
+}
+public static void Calculate(String s1,String s2)
+{
+
+if((s1.equals(s2))==true)
+{
+System.out.println("Two Strings are Equal");
+else
+System.out.println("Two Strings are not Equal");
+}
+class Compute
+{
+public static void main(string args[])
+{
+Scanner sc=new Scanner(system.in);
+Scanner sc=new Scanner(system.in);
+Scanner sc=new Scanner(system.in);
+}
+}
