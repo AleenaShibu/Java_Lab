bf19ea08f1536c24fe75ad402f8c44dd7b67fce3
Aleena Shibu <54793714+AleenaShibu@users.noreply.github.com>
2021-10-06 12:34:26 +0530

    Add files via upload

---
+++ Append.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+class Append
+{
+public static void main(String args[]) 
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the first number");
+int a=sc.nextInt();
+System.out.println("Enter the next number");
+int b=sc.nextInt();
+System.out.println(a+""+b);
+}
+}
